286e25b22148102e452f25f60439409331effa21
rjp <zimpenfish@gmail.com>
2010-12-13 16:48:10 +0000

    - fix up testconfig - testdata is now only for loading the initial database - tests.sh will create the initial redis dump if missing

--- tests.sh
+++ tests.sh
@@ -1,3 +1,17 @@
-# WARNING: this will blat redis database 2
-redis-cli < testdata
-./run-tests
+if [ ! -f testdata.rdb ]; then
+    echo "making initial test database"
+    redis-server testconfig &
+    redis-cli -p 9736 < testdata
+    mv testjunknull.rdb testdata.rdb
+fi
+
+# copy our fixed set of test data into place and start redis
+cp -f testdata.rdb testjunknull.rdb
+redis-server testconfig &
+env redisport=9736 ./run-tests
+env redisport=9736 ./run-tests-html > ua3ci.html
+# only useful on tegenaria
+if [ -f ~/Dropbox/Public ]; then
+    cp ua3ci.html ~/Dropbox/Public/
+fi
+echo shutdown | redis-cli -p 9736
