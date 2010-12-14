23c6c2a82429f7060f934167db491988960ee49d
rjp <zimpenfish@gmail.com>
2010-12-14 15:18:12 +0000

    - forget the fixed db file idea, stick with testdata - update tests.sh to load the db and generally work correctly

--- tests.sh
+++ tests.sh
@@ -1,17 +1,11 @@
-if [ ! -f testdata.rdb ]; then
-    echo "making initial test database"
-    redis-server testconfig &
-    redis-cli -p 9736 < testdata
-    mv testjunknull.rdb testdata.rdb
-fi
-
-# copy our fixed set of test data into place and start redis
-cp -f testdata.rdb testjunknull.rdb
 redis-server testconfig &
+sleep 2
+redis-cli -p 9736 < testdata >/dev/null
 env redisport=9736 ./run-tests
+redis-cli -p 9736 < testdata >/dev/null
 env redisport=9736 ./run-tests-html > ua3ci.html
 # only useful on tegenaria
-if [ -f ~/Dropbox/Public ]; then
+if [ -d ~/Dropbox/Public/ ]; then
     cp ua3ci.html ~/Dropbox/Public/
 fi
 echo shutdown | redis-cli -p 9736
