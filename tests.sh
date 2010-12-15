87aa53675abe466ffc45046ac70d4de7a2449c03
rjp <rjp@plugbox.lan>
2010-12-15 01:02:35 -0800

    - slight modifications for integrity CI reporting

--- tests.sh
+++ tests.sh
@@ -1,7 +1,14 @@
+if ! git submodule --quiet status; then
+    echo git submodules need sorting
+    git submodule init
+    git submodule update
+fi
+
 redis-server testconfig &
 sleep 2
 redis-cli -p 9736 < testdata >/dev/null
 env redisport=9736 ./run-tests
+result=$?
 redis-cli -p 9736 < testdata >/dev/null
 env redisport=9736 ./run-tests-html > ua3ci.html
 # only useful on tegenaria
@@ -9,3 +16,4 @@ if [ -d ~/Dropbox/Public/ ]; then
     cp ua3ci.html ~/Dropbox/Public/
 fi
 echo shutdown | redis-cli -p 9736
+exit $result
