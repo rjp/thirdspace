52e2f6c87f661de9cb211b56f8645ce530e8c8a4
rjp <zimpenfish@gmail.com>
2010-12-15 01:58:41 +0000

    - make tests.sh quieter - strip control sequences from output - only print output if we have test failures

--- tests.sh
+++ tests.sh
@@ -1,14 +1,16 @@
 missing=$(git submodule status | grep -c '^-')
 if [ $missing -gt 0 ]; then
-    echo git submodules need sorting
-    git submodule init
-    git submodule update
+    echo git submodules need sorting >> /dev/null
+    git submodule init >> /dev/null
+    git submodule update >> /dev/null
 fi
 
 redis-server testconfig &
 sleep 2
 redis-cli -p 9736 < testdata >/dev/null
-env redisport=9736 ./run-tests
+env redisport=9736 ./run-tests > testrun.log
 result=$?
-echo shutdown | redis-cli -p 9736
+echo shutdown | redis-cli -p 9736 >/dev/null
+# this strips the colouring because it confuses integrity
+if [ $result -gt 0 ]; then sed -e 's/[^m]*m//g' < testrun.log; fi
 exit $result
