8772987cd221a2e7e7ffc50257b36144c53673ce
rjp <zimpenfish@gmail.com>
2010-12-15 01:37:18 +0000

    - no need for run-tests-html now we have integrity doing CI

--- tests.sh
+++ tests.sh
@@ -2,10 +2,4 @@ redis-server testconfig &
 sleep 2
 redis-cli -p 9736 < testdata >/dev/null
 env redisport=9736 ./run-tests
-redis-cli -p 9736 < testdata >/dev/null
-env redisport=9736 ./run-tests-html > ua3ci.html
-# only useful on tegenaria
-if [ -d ~/Dropbox/Public/ ]; then
-    cp ua3ci.html ~/Dropbox/Public/
-fi
 echo shutdown | redis-cli -p 9736
