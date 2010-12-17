84c95d77317fa9d69dabc40ad6ff6ab836c24c96
rjp <zimpenfish@gmail.com>
2010-12-17 11:39:56 +0000

    - need to commit map.js for it to work, duh

---
+++ map.js
@@ -0,0 +1,25 @@
+var sys = require('sys');
+
+// perform asynchronous callbacks for each item in a list and then
+// pass the new list off to a final callback
+exports.map = function(list, each_callback, final_callback) {
+    // shortcut any processing if we've got an empty list
+    if (list === undefined || list === null || list.length === 0) {
+        final_callback(undefined, []);
+        return;
+    }
+    var ilist = new Array;
+    var lsize = list.length;
+    var mid_callback = function(err, val){
+        if (err) final_callback(err, undefined);
+        ilist.push(val);
+        sys.puts('pushed '+ilist.length+' / '+lsize);
+        if (ilist.length == lsize) {
+            final_callback(undefined, ilist);
+	    }
+    };
+    for(var i in list) {
+        each_callback(list[i], i, mid_callback);
+    }
+}
+
