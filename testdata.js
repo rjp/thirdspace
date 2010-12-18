8ec25d64b761c41791d2c042732f3804b5719146
rjp <zimpenfish@gmail.com>
2010-12-18 22:59:55 +0000

    - no point calling it threadId here when we call it thread everywhere else

--- testdata.js
+++ testdata.js
@@ -17,23 +17,23 @@ for(var i in exports.allfolders) {
 var test_messages = {
     "m_one": {
         "folder":"chat", "id":1, "epoch":1289914759,
-        "from":"rjp", "subject":"UA3", "threadId":1,
+        "from":"rjp", "subject":"UA3", "thread":1,
         "body":"UA3 is alive! Let there be rejoicing!"
     },
     "m_two": {
         "folder":"chat", "id":2, "epoch":1289914769,
         "from":"techno", "to":"rjp", "subject":"UA3",
-        "body":"Bring on the dancing horses!", "threadId":1,
+        "body":"Bring on the dancing horses!", "thread":1,
     },
     "m_three": {
         "folder":"test", "id":3, "epoch":1289914759,
         "from":"otama", "subject":"The taste of Dolphins",
-        "body":"Apparently they taste like fish.", "threadId":2
+        "body":"Apparently they taste like fish.", "thread":2
     },
     "m_four": {
         "folder":"chat", "id":4, "epoch":1289914759,
         "from":"parm", "subject":"FIRST!",
-        "body":"", "threadId":3
+        "body":"", "thread":3
     }
 };
 exports.messages = test_messages;
@@ -42,7 +42,7 @@ exports.messages = test_messages;
 var test_replies = {
     "r_one": {
         "folder":"chat", "id":11, "to":"rjp",
-        "from":"rjp", "subject":"UA3", "threadId":1,
+        "from":"rjp", "subject":"UA3", "thread":1,
         "body":"Testings"
     }
 };
