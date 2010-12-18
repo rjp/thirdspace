fb35277cde2b431000768ba8f0a04fd54516860d
rjp <zimpenfish@gmail.com>
2010-12-18 11:20:40 +0000

    - update the reply tests for new testdata

--- tests/07-reply.js
+++ tests/07-reply.js
@@ -6,10 +6,15 @@ var sys = require('sys');
 var testdata = require('../testdata.js');
 
 exports['reply-1'] = function(test){
-    tester.post('/message/1', {}, function(got, code){
+    tester.post('/message/1', {body:"Testings"}, function(got, code){
         test.expect(2);
         test.equal(code, 200, '200 OK');
-        test.deepEqual(got, testdata.messages.m_one, 'Matched M1');
+        var reply_epoch = got.epoch;
+        delete got.epoch; // ignore this for deepEqual
+        test.deepEqual(got, testdata.replies.r_one, 'Matched R1');
+        var now = parseInt(new Date().getTime() / 1000, 10);
+        // test that now - reply_epoch < 30ish
+        // test.within(30, now - reply_epoch, 'recent is ok');
         test.done();
     });
 };
