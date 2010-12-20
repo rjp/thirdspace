bba8c68abf923d6cfba22e7ebdb8c9b8952e80ab
rjp <zimpenfish@gmail.com>
2010-12-20 23:34:15 +0000

    - multiple users, new tester objects - currently broken because we have no good way of handling folders

--- tests/00-folders.js
+++ tests/00-folders.js
@@ -4,9 +4,19 @@ var tester = require('../httpclient');
 var sys = require('sys');
 var testdata = require('../testdata.js');
 
-exports['folders'] = function(test){
+exports['folders-1'] = function(test){
     var body = "";
-    tester.get('/folders', function(got, code){
+    tester.rjp.get('/folders', function(got, code){
+        test.expect(2);
+        test.equal(code, 200, '200 OK');
+        test.deepEqual(got, testdata.l_myfolders, "/folders");
+        test.done();
+    });
+};
+
+exports['folders-2'] = function(test){
+    var body = "";
+    tester.techno.get('/folders', function(got, code){
         test.expect(2);
         test.equal(code, 200, '200 OK');
         test.deepEqual(got, testdata.l_myfolders, "/folders");
