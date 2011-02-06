47d64db6775e829f68bc9c723bcb446f977a88ac
rjp <zimpenfish@gmail.com>
2011-02-06 16:16:54 +0000

    - record stderr into the log as well

--- tests.sh
+++ tests.sh
@@ -9,7 +9,7 @@ redis-server testconfig &
 sleep 2
 redis-cli -p 9736 < testdata >/dev/null
 env redisport=9736 node loaddata.js >> testrun.log
-env redisport=9736 ./run-tests > testrun.log
+env redisport=9736 ./run-tests 2>&1 > testrun.log
 result=$?
 echo shutdown | redis-cli -p 9736 >/dev/null
 # this strips the colouring because it confuses integrity
