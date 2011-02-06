3c03b1a6b162ec6d8e11dc15942cbb2053a3a7b1
rjp <zimpenfish@gmail.com>
2011-02-06 20:19:58 +0000

    - have to use temporary redis for CI testing

--- tests.sh
+++ tests.sh
@@ -1,5 +1,4 @@
-redisport=6379
-#redisport=9736
+redisport=9736
 missing=$(git submodule status | grep -c '^-')
 if [ $missing -gt 0 ]; then
     echo git submodules need sorting >> /dev/null
@@ -7,14 +6,14 @@ if [ $missing -gt 0 ]; then
     git submodule update >> /dev/null
 fi
 
-#redis-server testconfig &
+redis-server testconfig &
 sleep 2
 redis-cli -p $redisport < testdata >/dev/null
 :>testrun.log
 env redisport=$redisport node loaddata.js >> testrun.log
 env redisport=$redisport ./run-tests 2>&1 > testrun.log
 result=$?
-#echo shutdown | redis-cli -p $redisport >/dev/null
+echo shutdown | redis-cli -p $redisport >/dev/null
 # this strips the colouring because it confuses integrity
 if [ $result -gt 0 ]; then sed -e 's/[^m]*m//g' < testrun.log; fi
 exit $result
