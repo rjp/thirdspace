17848dbc717418ed50e41fa37c90d7826d7ff730
rjp <zimpenfish@gmail.com>
2011-02-14 20:10:33 +0000

    - test /thread/:id - should be the same for everyone at the moment

--- tests/03-threads.js
+++ tests/03-threads.js
@@ -5,21 +5,41 @@ var tester = require('../httpclient');
 var sys = require('sys');
 var testdata = require('../testdata.js');
 
-exports['thread-1'] = function(test){
-    tester.rjp.get('/thread/1', function(got, code){
-        test.expect(2);
-        test.equal(code, 200, '200 OK');
-        test.deepEqual(got, testdata.json.threads.t_1, 'Matched T1');
-        test.done();
-    });
+var threads = {
+    "1": ["m_one", "m_two", "m_ten"],
+    "2": ["m_three"],
+    "3": ["m_four"],
+    "4": ["m_five", "m_seven"]
 };
 
-exports['thread-2'] = function(test){
-    tester.rjp.get('/thread/2', function(got, code){
-        test.expect(2);
-        test.equal(code, 200, '200 OK');
-        test.deepEqual(got, testdata.json.threads.t_2, 'Matched T2');
-        test.done();
-    });
+// convert our thread shortcuts into full message lists
+var expected = [];
+for (var thread in threads) {
+    for (var message in threads[thread]) {
+        var id = threads[thread][message];
+        if (expected[thread] == undefined) { expected[thread] = []; }
+        var o = testdata.json.preload[id];
+        delete o['body'];
+        expected[thread].push(o);
+    }
 };
 
+function make_test(thread, who, wanted) {
+    var e = expected[thread];
+    return function(test) {
+	    tester[who].get('/thread/'+thread, function(got, code){
+	        test.expect(2);
+            test.equal(code, 200, '200 OK');
+	        test.deepEqual(got, e, "Thread "+thread+", "+who);
+	        test.done();
+	    });
+    }
+}
+
+var users = ['rjp','techno','lurker'];
+for(var i in users) {
+    var user = users[i];
+    for(var t in [1,2,3,4]) {
+        exports['thread-'+user+'-'+t] = make_test(1, user);
+    }
+}
